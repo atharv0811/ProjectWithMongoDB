e5e10179478a16b1b5dc2dc4d31c51a56f09d4ad
atharv0811 <karnekaratharv12@gmail.com>
2023-11-21 12:00:26 +0530

    Creating the Users table

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const errorController = require("./controllers/error");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 require("dotenv").config();
 const port = 3000;
 const app = express();
@@ -16,13 +17,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //     .then((user) => {
-  //         req.user = user;
-  //         next();
-  //     })
-  //     .catch(err => console.log(err));
-  next();
+  User.findById("655c49fbb21d4e9dd0cf38c0")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);

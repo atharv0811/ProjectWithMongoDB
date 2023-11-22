bdf7cf534984625fda9004f73fe2b5a1c34aea39
atharv0811 <karnekaratharv12@gmail.com>
2023-11-22 17:11:15 +0530

    Added user

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-// const mongoConnect = require("./util/database").mongoConnect;
 const mongoose = require("mongoose");
-// const User = require("./models/user");
+const User = require("./models/user");
 require("dotenv").config();
 const port = 3000;
 const app = express();
@@ -17,27 +16,35 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("655c49fbb21d4e9dd0cf38c0")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("655de4e16a1f3be33b3bc421")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// mongoConnect(() => {
-//   app.listen(port);
-// });
-
 mongoose
   .connect(process.env.MONGOURL)
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Atharv",
+          email: "atharv@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
     console.log("Connected");
     app.listen(port);
   })
--- controllers/admin.js
+++ controllers/admin.js
@@ -18,6 +18,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
     imageUrl: imageUrl,
+    userId: req.user._id,
   });
   product
     .save()
@@ -74,7 +75,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .select("title price -_id")
+    // .populate("userId")
     .then((products) => {
+      console.log(products);
       res.render("admin/products", {
         prods: products,
         pageTitle: "Admin Products",
